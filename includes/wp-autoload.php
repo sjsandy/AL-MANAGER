f2d6caeb36804ff2e1761e55cca4439c48197383
shawnsandy <shawnsandy04@gmail.com>
2013-01-28 01:15:05 -0500

    Modified - added autoload() to replace to many duplicate functions

--- includes/wp-autoload.php
+++ includes/wp-autoload.php
@@ -7,12 +7,26 @@
  */
 class wp_autoload {
 
-    private $class;
+    protected $class,
+            $paths,
+            $lowercase = false;
+
+    public function set_paths($paths) {
+        $this->paths = $paths;
+        return $this;
+    }
+
+    public function set_lowercase($lowercase) {
+        $this->lowercase = $lowercase;
+        return $this;
+    }
+
+
 
     public function set_class($class) {
         $this->class = $class;
+        return $this;
     }
-
 
     public function __construct() {
 
@@ -22,48 +36,74 @@ class wp_autoload {
      *
      * @return \wp_autoload
      */
-    static function factory(){
-        $factory = new wp_autoload;
-        $factory->load();
+    static function factory() {
+
+        $factory = new wp_autoload();
+        //$factory->load();
         return $factory;
+
     }
 
-
-
-    function load(){
-        spl_autoload_register(array('wp_autoload','classes'));
-        spl_autoload_register(array('wp_autoload','plugin_classes'));
-        spl_autoload_register(array('wp_autoload','theme_classes'));
+    function load() {
+        $paths[] = WP_PLUGIN_DIR . '/al-manager/classes/';
+        $paths[] = WP_PLUGIN_DIR . DIRECTORY_SEPARATOR ;
+        $paths[] = WP_CONTENT_DIR . '/themes/';
+        $this->paths = $paths;
+        $this->register();
+//
+//        spl_autoload_register(array($this, 'classes'));
+//        spl_autoload_register(array($this, 'classes_lowercase'));
+//        spl_autoload_register(array($this, 'plugin_classes'));
+//        spl_autoload_register(array($this, 'theme_classes'));
     }
 
-    public function classes($class){
-      $path = WP_PLUGIN_DIR . '/al-manager/classes/'.$class;
-      $this->get_path($path);
+    public function register(){
+        spl_autoload_register(array($this,'autoload'));
     }
 
+    public function autoload($class) {
+        $classname = $class.'.php';
+        foreach ($this->paths as $path) {
+            $_class = $path . $classname;
+            $this->get_path($_class, $this->lowercase);
+        }
 
-   public  function plugin_classes($class){
-      $path = WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $class;
-      $this->get_path($path);
     }
 
-   public  function theme_classes($class){
-        $path = WP_CONTENT_DIR .'/themes/'.$class;
+    public function classes($class) {
+        $path = WP_PLUGIN_DIR . '/al-manager/classes/' . $class;
         $this->get_path($path);
     }
 
-   private function get_path($path){
-        $path = str_replace('_', DIRECTORY_SEPARATOR, $path).'.php';
+    public function classes_lowercase($class) {
+        $path = WP_PLUGIN_DIR . '/al-manager/classes/' . $class;
+        $this->get_path($path, TRUE);
+    }
 
-        if(is_readable($path))
+    public function plugin_classes($class) {
+        $path = WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $class;
+        $this->get_path($path);
+    }
+
+    public function theme_classes($class) {
+        $path = WP_CONTENT_DIR . '/themes/' . $class;
+        $this->get_path($path);
+    }
+
+    private function get_path($path, $lowercase = false) {
+        $path = str_replace('_', DIRECTORY_SEPARATOR, $path);
+
+        if ($lowercase === TRUE)
+            $path = strtolower($path);
+        if (is_readable($path))
             require_once $path;
     }
 
-   public static function hello(){
-      echo "hello world";
+    public static function hello() {
+        echo "hello world";
     }
 
 }
 
-if(!class_exists('wp_autoload'))
-wp_autoload::factory();
+
+    wp_autoload::factory()->load();
